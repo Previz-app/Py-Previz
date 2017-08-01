a91384e35714f08a5be6cfc26c391b035b92c73c
Charles Flèche <charles@previz.co>
2017-08-01 13:38:01 +0200

    Add semantic_version third-party

--- scripts/sync_install.sh
+++ scripts/sync_install.sh
@@ -5,6 +5,7 @@ PLUGIN_SRC=$REPO_SRC/Py-Previz/Py-Previz
 PREVIZ_MODULE_SRC=$REPO_SRC/previz-python-api/previz
 REQUESTS_SRC=$REPO_SRC/Py-Previz/third-party/requests
 REQUESTS_TOOLBELT_SRC=$REPO_SRC/Py-Previz/third-party/requests_toolbelt
+SEMANTIC_VERSION_SRC=$REPO_SRC/Py-Previz/third-party/semantic_version
 
 PLUGIN_DST='Nialls-MacBook-Pro.local:/Users/charles/Library/Preferences/MAXON/CINEMA4D/plugins/Py-Previz'
 DEPS_DST=$PLUGIN_DST/res/lib/python/site-packages
@@ -15,4 +16,5 @@ rsync --archive --progress --human-readable \
     $PREVIZ_MODULE_SRC \
     $REQUESTS_SRC \
     $REQUESTS_TOOLBELT_SRC \
+    $SEMANTIC_VERSION_SRC \
     $DEPS_DST
